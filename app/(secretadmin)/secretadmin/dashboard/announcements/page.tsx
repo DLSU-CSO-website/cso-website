b4650b302826435a1071e7cd8a32b4063974262a
Ethea2 <nathandres2001@gmail.com>
2025-01-08 14:58:18 +0000

    fixed the cards a little

--- app/(secretadmin)/secretadmin/dashboard/announcements/page.tsx
+++ app/(secretadmin)/secretadmin/dashboard/announcements/page.tsx
@@ -1,16 +1,37 @@
 "use client"
 
 import AnnouncementDashCard from "@/components/AnnouncementDashCard"
+import { useAuth } from "@/hooks/useAuth"
 import useFetchData from "@/hooks/useFetchData"
 import { IAnnouncement } from "@/types/announcement.types"
 import { Loader } from "@mantine/core"
+import { notifications } from "@mantine/notifications"
+import { useRouter } from "next/navigation"
 import { useEffect } from "react"
 
 export default function AnnouncementDashboard() {
   const { data, loading, error } = useFetchData("/api/announcements")
+  const { user, loading: userLoading } = useAuth()
+  const router = useRouter()
+
   useEffect(() => {
-    console.log(data)
-  }, [data, loading, error])
+    console.log(user)
+    if (!userLoading) {
+      if (!user) {
+        router.push("/secretadmin")
+      }
+    }
+  }, [user, userLoading])
+
+  useEffect(() => {
+    if (error) {
+      notifications.show({
+        title: "Something went wrong :(",
+        message: data.message
+      })
+    }
+  }, [error])
+
   return (
     <div className="w-full h-screen bg-gradient-to-br from-white to-slate-200 flex justify-center items-center gap-10">
       {
--- components/AnnouncementDashCard.tsx
+++ components/AnnouncementDashCard.tsx
@@ -4,15 +4,14 @@ import { Card, Image } from "@mantine/core";
 export default function AnnouncementDashCard({ announcement }: { announcement: IAnnouncement }) {
   return (
     <Card shadow="md" radius="md" padding="lg" withBorder className="w-72 h-96 transition ease-in-out duration-200 hover:scale-110 cursor-pointer">
-      <Card.Section>
+      <Card.Section className="h-56">
         <Image
           src={announcement.image}
           radius="md"
-          fit="fill"
           alt={`${announcement.title} image`}
         />
       </Card.Section>
-      <div className="w-full h-full flex flex-col justify-start items-center">
+      <div className="w-full h-full flex flex-col justify-start items-center p-5">
         <span className="text-2xl text-center">{announcement.title}</span>
         <span className="text-gray-500">{announcement.body}</span>
       </div>
