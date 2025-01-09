b965247ea2ceddd93502f1a542c81d8cd1ddaaae
Jeyell 26 <john_louise_bungay@dlsu.edu.ph>
2025-01-09 14:37:23 +0800

    Added fake inputs for build

---
+++ app/(secretadmin)/secretadmin/dashboard/organizations/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const OrganizationsPage: React.FC = () => {
+    return (
+        <div>
+            <h1>Organizations Dashboard</h1>
+            <p>Welcome to the organizations dashboard. Here you can manage all the organizations.</p>
+        </div>
+    );
+};
+
+export default OrganizationsPage;
