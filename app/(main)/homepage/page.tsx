b060bbd9e2a260aa0ffd963beb1072fcd69beeef
pattyarao-dev <kkenmapagmahal@gmail.com>
2025-01-12 19:54:23 +0800

    finished homepage design

--- app/(main)/homepage/page.tsx
+++ app/(main)/homepage/page.tsx
@@ -6,9 +6,36 @@ import useFetchData from "@/hooks/useFetchData";
 import { IAnnouncement } from "@/types/announcement.types";
 import AnnouncementHomeCard from "@/components/AnnouncementHomeCard";
 import { Loader } from "@mantine/core";
+import { useState } from "react";
 
 const Homepage = () => {
   const { data, loading } = useFetchData("/api/announcements");
+  const organizations = [
+    { name: "Alliance of Science Organizations", images: "image set 1" },
+    {
+      name: "College of Liberal Arts Professional Organizations",
+      images: "image set 2",
+    },
+    {
+      name: "Enginering Alliance Geared Towards Excellence",
+      images: "image set 3",
+    },
+    {
+      name: "Alliance of Special Integrated & Socio-Civic Organizations",
+      images: "image set 4",
+    },
+    {
+      name: "Alliance of Professional Organizations of Business and Economics",
+      images: "image set 5",
+    },
+  ];
+
+  const [selectedOrg, setSelectedOrg] = useState<string | null>(null);
+
+  const selectedOrgImage = organizations.find(
+    (org) => org.name === selectedOrg,
+  )?.images;
+
   return (
     <main className="w-full flex flex-col items-center justify-center">
       <section className="section-layout gradient-background">
@@ -45,6 +72,39 @@ const Homepage = () => {
               <AnnouncementHomeCard key={key} announcement={announcement} />
             ))
           )}
+        </div>
+      </section>
+      <section className="section-layout p-10 flex justify-center gradient-background-light">
+        <div className="w-full flex items-center justify-center shadow-inner drop-shadow-lg">
+          <div className="w-[25%] h-screen p-10 gradient-background flex flex-col justify-center gap-10">
+            <div className="w-full flex flex-col gap-2">
+              <div>
+                <h1 className="text-white font-bold">Our Organizations</h1>
+                <h2 className="text-secondary font-bold">
+                  48 Organizations. 1 CSO
+                </h2>
+              </div>
+              <hr />
+            </div>
+            <div className="w-full flex flex-col justify-start gap-6">
+              {organizations.map((item) => (
+                <p
+                  key={item.name}
+                  className="px-4 py-2 text-white font-semibold rounded-lg cursor-pointer hover:bg-white/60 transition-all ease-in-out duration-300"
+                  onMouseEnter={() => setSelectedOrg(item.name)}
+                >
+                  {item.name}
+                </p>
+              ))}
+            </div>
+          </div>
+          <div className="w-[75%] h-screen bg-[#F5F5E9] flex items-center justify-center">
+            {selectedOrgImage ? (
+              <p>{selectedOrgImage}</p>
+            ) : (
+              <p>No organization selected</p>
+            )}
+          </div>
         </div>
       </section>
     </main>
