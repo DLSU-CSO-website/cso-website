1b7c2bf05cd41993b10f8e2260855e46665d0fa4
pattyarao-dev <kkenmapagmahal@gmail.com>
2025-05-18 10:05:34 +0800

    created responsive navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,9 @@
+"use client";
+
+import { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
+import { IoMdMenu, IoMdClose } from "react-icons/io";
 
 const Navbar = () => {
   const pagesRoute = [
@@ -21,23 +25,55 @@ const Navbar = () => {
     },
   ];
 
+  const [showNavbar, setShowNavbar] = useState(false);
+
   return (
-    <div className="z-40 top-0 sticky w-full h-fit px-6 py-4 bg-primary/80 backdrop-blur drop-shadow-md flex items-center justify-between">
-      <div>
-        <Image src="/cso-logo-green.png" width={50} height={50} alt="logo" />
+    <>
+      <div className="z-40 top-0 sticky w-full h-fit px-6 py-4 bg-primary/80 backdrop-blur drop-shadow-md flex items-center justify-between">
+        <div>
+          <Image src="/cso-logo-green.png" width={50} height={50} alt="logo" />
+        </div>
+        <div className="hidden w-fit md:flex items-center gap-10">
+          {pagesRoute.map((page, key: number) => (
+            <Link
+              href={`${page.route}`}
+              key={key}
+              className="text-secondary uppercase font-semibold cursor-pointer"
+            >
+              {page.pageName}
+            </Link>
+          ))}
+        </div>
+        <div className="flex w-fit md:hidden items-center gap-10">
+          {showNavbar ? (
+            <IoMdClose
+              onClick={() => setShowNavbar(!showNavbar)}
+              className="text-2xl text-white font-bold"
+            />
+          ) : (
+            <IoMdMenu
+              onClick={() => setShowNavbar(!showNavbar)}
+              className="text-2xl text-white font-bold"
+            />
+          )}
+        </div>
       </div>
-      <div className="w-fit flex items-center gap-10">
-        {pagesRoute.map((page, key: number) => (
-          <Link
-            href={`${page.route}`}
-            key={key}
-            className="text-secondary uppercase font-semibold cursor-pointer"
-          >
-            {page.pageName}
-          </Link>
-        ))}
-      </div>
-    </div>
+      {showNavbar === true && (
+        <div className="z-20 fixed w-full min-h-screen p-10 bg-white/80 backdrop-blur flex justify-start">
+          <div className="w-full h-full flex flex-col gap-10">
+            {pagesRoute.map((page, key) => (
+              <Link
+                href={page.route}
+                key={key}
+                className="w-full text-3xl md:text-base text-secondary uppercase font-semibold cursor-pointer hover:bg-green"
+              >
+                {page.pageName}
+              </Link>
+            ))}
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
