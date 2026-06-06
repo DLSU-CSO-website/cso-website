9c0cc1d0de110df6ead8ba7072c91996004b95c4
Ethea2 <nathandres2001@gmail.com>
2026-06-06 23:23:38 +0800

    patch: quick fix on delete error

--- app/(secretadmin)/secretadmin/dashboard/announcements/[id]/page.tsx
+++ app/(secretadmin)/secretadmin/dashboard/announcements/[id]/page.tsx
@@ -1,10 +1,17 @@
-"use client"
+"use client";
 
 import useAnnouncementDelete from "@/hooks/useAnnouncementDelete";
 import useAnnouncementEdit from "@/hooks/useAnnouncementEdit";
 import { useAuth } from "@/hooks/useAuth";
 import useFetchData from "@/hooks/useFetchData";
-import { Button, Input, Image, FileButton, FileInput, Modal } from "@mantine/core";
+import {
+  Button,
+  Input,
+  Image,
+  FileButton,
+  FileInput,
+  Modal,
+} from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
 import { notifications } from "@mantine/notifications";
 import { RichTextEditor } from "@mantine/tiptap";
@@ -13,140 +20,155 @@ import Link from "@tiptap/extension-link";
 import { useEditor } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 import { useParams, useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const ViewAnnouncement = () => {
-  const { user, loading: userLoading } = useAuth()
-  const router = useRouter()
+  const { user, loading: userLoading } = useAuth();
+  const router = useRouter();
   const { id } = useParams(); // Get the id from the URL
-  const { data, loading } = useFetchData("/api/announcements/" + id)
-  const [image, setImage] = useState<string>("")
-  const [title, setTitle] = useState<string>("")
-  const [notifId, setNotifId] = useState<string>("")
-  const [deleteNotifId, setDeleteNotifId] = useState<string>("")
+  const { data, loading } = useFetchData("/api/announcements/" + id);
+  const [image, setImage] = useState<string>("");
+  const [title, setTitle] = useState<string>("");
+  const notifIdRef = useRef<string>("");
+  const deleteNotifIdRef = useRef<string>("");
+  const [body, setBody] = useState<string>("");
+  const [imageFile, setImageFile] = useState<File | null>();
+  const {
+    success,
+    loading: editLoading,
+    error: editError,
+    uploadEdit,
+  } = useAnnouncementEdit();
+  const {
+    success: deleteSuccess,
+    loading: deleteLoading,
+    error: deleteError,
+    deleteUpload,
+  } = useAnnouncementDelete();
 
-  const [body, setBody] = useState<string>("")
-  const [imageFile, setImageFile] = useState<File | null>()
-  const { success, loading: editLoading, error: editError, uploadEdit } = useAnnouncementEdit()
-  const { success: deleteSuccess, loading: deleteLoading, error: deleteError, deleteUpload } = useAnnouncementDelete()
-
-  const [opened, { open, close }] = useDisclosure(false)
-  const [deleteString, setDeleteString] = useState<string>("")
+  const [opened, { open, close }] = useDisclosure(false);
+  const [deleteString, setDeleteString] = useState<string>("");
 
   const editor = useEditor({
-    extensions: [
-      StarterKit,
-      Link,
-    ],
+    extensions: [StarterKit, Link],
     immediatelyRender: false,
     content: body,
     onUpdate: ({ editor }) => {
-      setBody(editor.getHTML())
-    }
-  })
+      setBody(editor.getHTML());
+    },
+  });
 
   useEffect(() => {
     if (editLoading) {
-      setNotifId(notifications.show({
+      notifIdRef.current = notifications.show({
         loading: true,
         title: "Making edits to your Announcement right now! ",
-        message: "Just sit down and chill we're doing cool things in the background!",
+        message:
+          "Just sit down and chill we're doing cool things in the background!",
         autoClose: false,
         withCloseButton: false,
-        position: "bottom-center"
-      }))
+        position: "bottom-center",
+      });
     }
 
     if (!editLoading) {
       if (editError) {
         notifications.update({
-          id: notifId,
+          id: notifIdRef.current,
           title: "Oh no something went wrong!",
           message: editError,
           autoClose: 2000,
           color: "red",
-          position: "bottom-center"
-        })
+          position: "bottom-center",
+        });
       }
       if (success) {
         notifications.update({
-          id: notifId,
+          id: notifIdRef.current,
           title: "Edit Successful!",
           message: "Your awesome edited announcement is now up in the web!",
           autoClose: 2000,
           loading: false,
           icon: <IconCheck size={18} />,
-          position: "bottom-center"
-        })
-        router.push("/secretadmin/dashboard/announcements")
+          position: "bottom-center",
+        });
+        router.push("/secretadmin/dashboard/announcements");
       }
     }
 
-
-    console.log("Successful delete: ", deleteSuccess)
+    console.log("Successful delete: ", deleteSuccess);
     if (deleteLoading) {
-      setDeleteNotifId(notifications.show({
+      deleteNotifIdRef.current = notifications.show({
         loading: true,
         title: "Deleting your Announcement right now! ",
-        message: "Just sit down and chill we're doing cool things in the background!",
+        message:
+          "Just sit down and chill we're doing cool things in the background!",
         autoClose: false,
         withCloseButton: false,
-        position: "bottom-center"
-      }))
+        position: "bottom-center",
+      });
     }
 
     if (!deleteLoading) {
       if (deleteError) {
         notifications.update({
-          id: deleteNotifId,
+          id: deleteNotifIdRef.current,
           title: "Oh no something went wrong!",
           message: deleteError,
           autoClose: 2000,
           color: "red",
-          position: "bottom-center"
-        })
+          position: "bottom-center",
+        });
       }
       if (deleteSuccess) {
         notifications.update({
-          id: deleteNotifId,
+          id: deleteNotifIdRef.current,
           title: "Delete Successful!",
           message: "Your awesome announcement is now gone from the web!",
           autoClose: 2000,
           loading: false,
           icon: <IconCheck size={18} />,
-          position: "bottom-center"
-        })
-        router.push("/secretadmin/dashboard/announcements")
+          position: "bottom-center",
+        });
+        router.push("/secretadmin/dashboard/announcements");
       }
     }
-  }, [success, editLoading, editError, deleteSuccess, deleteError, deleteLoading, router, notifId, deleteNotifId])
+  }, [
+    success,
+    editLoading,
+    editError,
+    deleteSuccess,
+    deleteError,
+    deleteLoading,
+    router,
+  ]);
 
   useEffect(() => {
     if (!userLoading) {
       if (!user) {
-        router.push("/secretadmin")
+        router.push("/secretadmin");
       }
     }
-  }, [user, userLoading, router])
+  }, [user, userLoading, router]);
 
   useEffect(() => {
     if (imageFile) {
-      const objectUrl = URL.createObjectURL(imageFile)
-      setImage(objectUrl)
+      const objectUrl = URL.createObjectURL(imageFile);
+      setImage(objectUrl);
+      return () => URL.revokeObjectURL(objectUrl);
     } else {
-      setImage("")
+      setImage("");
     }
-  }, [imageFile])
-
+  }, [imageFile]);
 
   useEffect(() => {
     if (!loading) {
       if (data) {
-        setImage(data.image)
-        setTitle(data.title)
-        setBody(data.body)
+        setImage(data.image);
+        setTitle(data.title);
+        setBody(data.body);
         if (editor && data.body) {
-          editor.commands.setContent(data.body)
+          editor.commands.setContent(data.body);
         }
       }
       // if (!data) {
@@ -160,10 +182,10 @@ const ViewAnnouncement = () => {
       //   router.push("/secretadmin/dashboard/announcements")
       // }
     }
-  }, [data, editor, loading])
+  }, [data, editor, loading]);
 
   const saveChanges = () => {
-    const formDataChanges = new FormData()
+    const formDataChanges = new FormData();
 
     if (title === "") {
       notifications.show({
@@ -171,9 +193,9 @@ const ViewAnnouncement = () => {
         message: "Please add a title!",
         autoClose: 2000,
         color: "red",
-        position: "bottom-center"
-      })
-      return
+        position: "bottom-center",
+      });
+      return;
     }
 
     if (editor?.getText() === "") {
@@ -182,25 +204,24 @@ const ViewAnnouncement = () => {
         message: "Please make sure there's something in the body!",
         autoClose: 2000,
         color: "red",
-        position: "bottom-center"
-      })
-      return
+        position: "bottom-center",
+      });
+      return;
     }
 
-
-    formDataChanges.append("title", title)
-    formDataChanges.append("body", body)
+    formDataChanges.append("title", title);
+    formDataChanges.append("body", body);
     if (imageFile) {
-      formDataChanges.append("image", imageFile)
+      formDataChanges.append("image", imageFile);
     }
-    formDataChanges.append("id", String(id))
+    formDataChanges.append("id", String(id));
 
     if (!user) {
-      return
+      return;
     }
 
-    uploadEdit(formDataChanges, user.token)
-  }
+    uploadEdit(formDataChanges, user.token);
+  };
 
   const deleteAnnouncement = () => {
     if (deleteString !== "DELETE") {
@@ -209,33 +230,66 @@ const ViewAnnouncement = () => {
         message: "Please type DELETE for full confirmation of this action.",
         autoClose: 2000,
         color: "red",
-        position: "bottom-center"
-      })
-      return
+        position: "bottom-center",
+      });
+      return;
     }
     if (!user) {
-      return
+      return;
     }
-    deleteUpload(String(id), user.token)
-  }
+    deleteUpload(String(id), user.token);
+  };
 
   return (
     <>
-      <Modal opened={opened} onClose={close} title={`Delete '${title}'`} centered className="text-center">
+      <Modal
+        opened={opened}
+        onClose={close}
+        title={`Delete '${title}'`}
+        centered
+        className="text-center"
+      >
         <div className="flex flex-col gap-5 text-center">
-          <Input.Wrapper label="Are you sure about this? This is irreversible" description="To delete please type DELETE" className="w-full">
-            <Input value={deleteString} onChange={(e) => setDeleteString(e.target.value)} placeholder="Type delete here..." className="w-full h-full" />
+          <Input.Wrapper
+            label="Are you sure about this? This is irreversible"
+            description="To delete please type DELETE"
+            className="w-full"
+          >
+            <Input
+              value={deleteString}
+              onChange={(e) => setDeleteString(e.target.value)}
+              placeholder="Type delete here..."
+              className="w-full h-full"
+            />
           </Input.Wrapper>
-          <Button color="red" onClick={deleteAnnouncement}>DELETE</Button>
+          <Button color="red" onClick={deleteAnnouncement}>
+            DELETE
+          </Button>
         </div>
       </Modal>
       <div className="w-full min-h-screen flex justify-center items-center py-10">
         <div className="w-1/2 h-full flex flex-col gap-5 justify-center items-center">
-          <Input.Wrapper label="Title" description="Please write the title of the announcement here!" className="w-full">
-            <Input value={title} onChange={(e) => setTitle(e.target.value)} placeholder="Title goes here..." className="w-full h-full" />
+          <Input.Wrapper
+            label="Title"
+            description="Please write the title of the announcement here!"
+            className="w-full"
+          >
+            <Input
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              placeholder="Title goes here..."
+              className="w-full h-full"
+            />
           </Input.Wrapper>
-          <Input.Wrapper className="w-full h-1/3" label="Body" description="Please write the announcement body here.">
-            <RichTextEditor editor={editor} className="h-full w-full overflow-scroll">
+          <Input.Wrapper
+            className="w-full h-1/3"
+            label="Body"
+            description="Please write the announcement body here."
+          >
+            <RichTextEditor
+              editor={editor}
+              className="h-full w-full overflow-scroll"
+            >
               <RichTextEditor.Toolbar sticky>
                 <RichTextEditor.ControlsGroup>
                   <RichTextEditor.Bold />
@@ -272,29 +326,38 @@ const ViewAnnouncement = () => {
 
               <RichTextEditor.Content />
             </RichTextEditor>
-
           </Input.Wrapper>
           <div className="w-full h-1/4 mt-10">
-            {
-              image ?
-                <>
-                  <Image className="object-center w-full h-full" fit="cover" src={image} alt={image} />
-                  <div className="w-full mt-2 flex justify-center items-center">
-                    <FileButton accept="image/png,image/jpeg,image/jpg" onChange={setImageFile}>
-                      {
-                        (props) => (
-                          <Button {...props}>Change Image</Button>
-                        )
-                      }
-                    </FileButton>
-                  </div>
-                </>
-                :
-                <FileInput accept="image/png,image/jpeg,image/jpg" className="w-full h-full flex justify-center items-center" onChange={setImageFile} placeholder={<IconFilePlus />} />
-            }
+            {image ? (
+              <>
+                <Image
+                  className="object-center w-full h-full"
+                  fit="cover"
+                  src={image}
+                  alt={image}
+                />
+                <div className="w-full mt-2 flex justify-center items-center">
+                  <FileButton
+                    accept="image/png,image/jpeg,image/jpg"
+                    onChange={setImageFile}
+                  >
+                    {(props) => <Button {...props}>Change Image</Button>}
+                  </FileButton>
+                </div>
+              </>
+            ) : (
+              <FileInput
+                accept="image/png,image/jpeg,image/jpg"
+                className="w-full h-full flex justify-center items-center"
+                onChange={setImageFile}
+                placeholder={<IconFilePlus />}
+              />
+            )}
           </div>
           <div className="w-full flex justify-between items-center">
-            <Button color="red" onClick={open}>Delete</Button>
+            <Button color="red" onClick={open}>
+              Delete
+            </Button>
             <Button onClick={saveChanges}>Save Changes</Button>
           </div>
         </div>
